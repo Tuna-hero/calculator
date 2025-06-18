f531d7a9ddb01cf84fae98c44e9748361a452571
Tuna Kahraman <165674271+Tuna-hero@users.noreply.github.com>
2025-06-18 10:53:19 +0300

    Delete calculator directory

--- calculator/src/app/pipes/double-digit.pipe.ts
+++
@@ -1,8 +0,0 @@
-import { Pipe, PipeTransform } from '@angular/core';
-
-@Pipe({ name: 'doubleDigit' })
-export class DoubleDigitPipe implements PipeTransform {
-  transform(value: string | number): string {
-    return value.toString().padStart(2, '0');
-  }
-}
